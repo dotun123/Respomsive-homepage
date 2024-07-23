1a6079aec8345f31d15232cadce425c6a4160b9e
justice agunkejoye <justiceagunkejoye@gmail.com>
2024-07-23 12:02:03 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import { Header,Testimonials,Footer,Topics,Info ,Blog} from './components'
+function App() {
+ 
+
+  return (
+    <>
+     <Header/>
+     <Topics/>
+     <Info/>
+     <Blog/>
+     <Testimonials/>
+     <Footer/>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import Header from "./Header/Header"
+import Testimonials from "./Testimonials/Testimonials";
+import Topics from "./Topics/Topics";
+import Info from "./Info/Info";
+import Footer from "./Footer/Footer";
+import Blog from "./Blog/Blog"
+
+export{
+    Header,
+    Testimonials,
+    Topics,
+    Info,
+    Footer,
+    Blog
+
+}
